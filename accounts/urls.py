8b5e60a4e1b8c4aca66f6f29903096c4beb80cd8
Noah Dara <noah@petalmafrica.com>
2026-03-10 20:58:04 +0200

    ACcounts settings

--- accounts/urls.py
+++ accounts/urls.py
@@ -29,8 +29,8 @@ urlpatterns = [
     path('logout/', views. logout_view, name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset_request'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='mail_list/emails/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='mail_list/emails/password_reset_complete.html'), name='password_reset_complete'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='accounts/password_change.html', success_url='/home'), name="password_change"),
     path('reset/', CustomPasswordResetView.as_view(), name='password_reset'),
 ]
